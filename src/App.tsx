170aa994c7cd1b87f767293097029e7e392c3ce9
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2023-11-02 11:14:42 +0100

    added route for errorfallback

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./components/MainPage.tsx";
 import "./index.css";
 import ShareWeek from "./components/ShareWeek.tsx";
+import ErrorFallback from "./components/ErrorFallback.tsx";
 
 function App() {
   return (
     <>
-          <BrowserRouter>
-
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/shared/:id" element={<ShareWeek />} />
-      </Routes>
-</BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/shared/:id" element={<ShareWeek />} />
+          <Route path="*" element={<ErrorFallback />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
